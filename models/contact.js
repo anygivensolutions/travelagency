3a668289a15034484f16d1751f684331f6eac9b7
Dom <anygivensolutions@users.noreply.github.com>
2019-01-07 17:14:32 -0800

    Add contact model

---
+++ models/contact.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+
+var contactSchema = new mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    email: String,
+    homePhone: String,
+    cell: String,
+    address: String,
+    city: String,
+    state: String,
+    zipcode: String
+});
+
+module.exports = mongoose.model('Contact', contactSchema);
