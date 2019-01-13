bea9feaa7c579d8e914512ae204b65d2381c089b
Dom <anygivensolutions@users.noreply.github.com>
2019-01-13 11:17:49 -0800

    Add faker to seed DB

--- seedDB.js
+++ seedDB.js
@@ -1,27 +1,33 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 const Contact = require('./models/contact');
-
+const Faker = require('faker');
 mongoose.Promise = global.Promise
 mongoose.connect(`mongodb://${process.env.USERNAME}:${process.env.PSWD}@localhost/${process.env.DB}?authSource=${process.env.SOURCE}`, {useNewUrlParser: true});
 
 
 let myContact = new Contact ({
-    firstName: 'Kris',
-    lastName: 'Carter',
-    email: 'youknow@solutions.com',
-    homePhone: '70684848888',
-    cell: '8788843838',
-    address: '421 temple dr.',
-    city: 'fortson',
-    state: 'IA',
-    zipcode: '83991'
+    firstName: Faker.name.firstName(),
+    lastName: Faker.name.lastName(),
+    email: Faker.internet.email(),
+    homePhone: Faker.phone.phoneNumber(),
+    cell: Faker.phone.phoneNumber(),
+    address: Faker.address.streetAddress(),
+    city: Faker.address.city(),
+    state: Faker.address.state(),
+    zipcode: Faker.address.zipCode()
 });
+
+
+var fakeContacts = [];
+var amountOfContacts = 1000;
 
 myContact.save((err) => {
     if(err) {
-        console.log(err);
+     console.log(err);
     } else {
         console.log('===== Saved another Contact =====!');
+        console.log(myContact.firstName);
     }
 });
+
