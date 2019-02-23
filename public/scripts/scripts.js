4ce6451f198a5884c17f3e29ff012e46b25a8da6
Dom <anygivensolutions@users.noreply.github.com>
2019-02-23 14:05:08 -0800

    Add scripts

---
+++ public/scripts/scripts.js
@@ -0,0 +1,9 @@
+function confirmDeletion () {
+  document.getElementByClassName('confirmDelete').addEventListener('click', function (e) {
+  e.preventDefault()
+    alert('is it a yes?')
+  }
+
+  )
+}
+
