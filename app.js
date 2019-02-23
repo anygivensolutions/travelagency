562937bfeee9896bd6a5d6b134f99c0224024f61
Dom <anygivensolutions@users.noreply.github.com>
2019-02-23 14:04:26 -0800

    Add user authentication

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const hbs = require('hbs')
 const methodOverride = require('method-override')
 const passport = require('passport')
 const LocalStrategy = require('passport-local')
+const session = require('express-session')
 const passportLocalMongoose = require('passport-local-mongoose')
 const serveStatic = require('serve-static')
 const expressSanitize = require('express-sanitizer')
@@ -12,13 +13,18 @@ const User = require('./models/user')
 const Contact = require('./models/contact')
 const app = express()
 require('dotenv').config()
+app.use(require ('express-session')({
+  secret: `${process.env.SAUCE}`,
+  resave: false,
+  saveUninitialized: false
+
+}))
 // db config
 mongoose.Promise = global.Promise
 mongoose.connect(`mongodb://${process.env.USERNAME}:${process.env.PSWD}@localhost/${process.env.DB}?authSource=${process.env.SOURCE}`, { useNewUrlParser: true })
 // app config
 app.set('view engine', 'hbs')
 app.use(passport.initialize())
-app.use(passport.session())
 app.use(passport.session())
 passport.use(new LocalStrategy(User.authenticate()))
 passport.serializeUser(User.serializeUser())
@@ -31,16 +37,64 @@ hbs.registerPartials(__dirname + '/views/partials')
 // serve public routes
 app.use(serveStatic('public'))
 
-app.get('/', (req, res) => {
-  res.render('landing')
+//app.get('/', (req, res) => {
+  //res.render('landing')
+//})
+// admin entry route
+app.get('/',isLoggedIn, (req, res) => {
+  res.render('dashboard/landing')
 })
+
+//
+// sign up form
+app.get('/register', (req, res) => {
+  res.render('dashboard/register')
+})
+// handle user sign up
+app.post('/register', (req, res) => {
+  User.register(new User({ username: req.body.username }), req.body.password, function (err, user) {
+    if (err) {
+      console.log(err)
+      return res.render('dashboard/register')
+    } else {
+      passport.authenticate('local')(req, res, function () {
+        res.redirect('/')
+      })
+    }
+  })
+})
+
+// login routes
+app.get('/login', (req, res) => {
+  res.render('dashboard/login')
+})
+
+app.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureREdirect: '/login'
+}), function (req, res) {
+
+})
+
+// logout 
+app.get('/logout', (req, res) => {
+  req.logout()
+  res.redirect('/')
+})
+
+function isLoggedIn (req, res, next) {
+  if (req.isAuthenticated()) {
+    return next()
+  }
+  res.redirect('/login')
+}
 // show contacts list
 app.get('/contacts', (req, res) => {
-  Contact.find().sort({firstName:1}).exec(function(err, allContacts) {
-    if  (err)  {
+  Contact.find().sort({ firstName: 1 }).exec(function (err, allContacts) {
+    if (err) {
       console.log(err)
     } else {
-      res.render('contacts/index',{
+      res.render('dashboard/contacts/index', {
         pageTitle: 'Address Book',
         contacts: allContacts
       })
@@ -48,43 +102,43 @@ app.get('/contacts', (req, res) => {
   })
 })
 
-//show new contact form
+// show new contact form
 app.get('/contacts/new', (req, res) => {
-  res.render('contacts/contactForm')
+  res.render('dashboard/contacts/contactForm')
 })
-//create new contact 
+// create new contact
 app.post('/contacts/new', (req, res) => {
-  Contact.create(req.body.contact, function(err, newContact){
-    if(err) {
+  Contact.create(req.body.contact, function (err, newContact) {
+    if (err) {
       console.log(err)
-      res.render('contacts/index')
+      res.render('dashboard/contacts/index')
     } else {
       res.redirect('/contacts')
       console.log(newContact)
     }
   })
 })
-//show one contact 
+// show one contact
 app.get('/contacts/:id', (req, res) => {
-  Contact.findById(req.params.id).populate('contact').exec(function(err, foundContact){
-    if(err) {
-      console.log(err)
-    } else {
-      console.log(foundContact)
-      res.render('contacts/show', {
-        contact: foundContact
-      })
-    }
-  })
-});
-// edit contact
-app.get('/contacts/:id/edit', (req, res) => {
-  Contact.findById(req.params.id).populate('contact').exec(function(err, foundContact){
+  Contact.findById(req.params.id).populate('contact').exec(function (err, foundContact) {
     if (err) {
       console.log(err)
     } else {
       console.log(foundContact)
-      res.render('contacts/edit', {
+      res.render('dashboard/contacts/show', {
+        contact: foundContact
+      })
+    }
+  })
+})
+// edit contact
+app.get('/contacts/:id/edit', (req, res) => {
+  Contact.findById(req.params.id).populate('contact').exec(function (err, foundContact) {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log(foundContact)
+      res.render('dashboard/contacts/edit', {
         contact: foundContact
       })
     }
@@ -112,8 +166,11 @@ app.delete('/contacts/:id', (req, res) => {
   })
 })
 app.get('/paymentbook', (req, res) => {
-  res.render('paymentbook/index')
+  res.render('dashboard/paymentbook/index')
 })
+
+
+
 
 app.listen(`${process.env.PORT}`, () => {
   console.log('=====Serving Wanda\'s Tours =====')
